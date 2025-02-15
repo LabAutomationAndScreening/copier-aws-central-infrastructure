4b6143e896a2327c9ca2e471cc07ed65e2bf982b
Eli Fine <ejfine@gmail.com>
2025-02-15 18:35:31 +0000

    pylint

--- template/src/aws_central_infrastructure/artifact_stores/lib/program.py
+++ template/src/aws_central_infrastructure/artifact_stores/lib/program.py
@@ -1,17 +1,9 @@
 import logging
 
-from ephemeral_pulumi_deploy import get_aws_account_id
-from ephemeral_pulumi_deploy import get_config
-from pulumi import export
-
 logger = logging.getLogger(__name__)
 
 
 def pulumi_program() -> None:
     """Execute creating the stack."""
-    aws_account_id = get_aws_account_id()
-    export("aws-account-id", aws_account_id)
-    env = get_config("proj:env")
-    export("env", env)
 
     # Create Resources Here
