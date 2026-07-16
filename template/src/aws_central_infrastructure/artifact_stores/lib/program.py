9050d8156b8f1b283a3fd587b0d28f4ad0b17587
Eli Fine <eli88fine@gmail.com>
2026-07-16 19:52:07 +0000

    external creds

--- template/src/aws_central_infrastructure/artifact_stores/lib/program.py
+++ template/src/aws_central_infrastructure/artifact_stores/lib/program.py
@@ -8,11 +8,14 @@ from aws_central_infrastructure.iac_management.lib import GITHUB_OIDC_URL
 
 from ..ami_sharing import define_image_builders
 from ..container_registries import define_container_registries
+from ..external_creds import define_external_creds
 from ..internal_packages import create_internal_packages_configs
 from .code_artifact import CentralCodeArtifact
 from .code_artifact import RepoPackageClaims
 from .ecr import EcrConfig
 from .ecr import create_ecrs
+from .external_creds import ExternalCredConfig
+from .external_creds import create_external_creds
 from .image_builder import ImageBuilderConfig
 from .image_builder import create_image_builders
 from .ssm_buckets import DistributorPackagesBucket
@@ -29,7 +32,8 @@ def pulumi_program() -> None:
     manual_artifacts_bucket = ManualArtifactsBucket()
     distributor_packages_bucket = DistributorPackagesBucket()
     create_ssm_bucket_ssm_params(
-        distributor_packages_bucket=distributor_packages_bucket, manual_artifacts_bucket=manual_artifacts_bucket
+        distributor_packages_bucket=distributor_packages_bucket,
+        manual_artifacts_bucket=manual_artifacts_bucket,
     )
     export(
         "manual-artifacts-bucket-name", manual_artifacts_bucket.bucket.bucket_name
@@ -47,4 +51,11 @@ def pulumi_program() -> None:
     ecr_repos: list[EcrConfig] = []
     central_infra_oidc_provider_arn = get_open_id_connect_provider(url=GITHUB_OIDC_URL).arn
     define_container_registries(ecr_repos)
-    create_ecrs(ecr_configs=ecr_repos, central_infra_oidc_provider_arn=central_infra_oidc_provider_arn, org_id=org_id)
+    create_ecrs(
+        ecr_configs=ecr_repos,
+        central_infra_oidc_provider_arn=central_infra_oidc_provider_arn,
+        org_id=org_id,
+    )
+    external_creds: list[ExternalCredConfig] = []
+    define_external_creds(external_creds)
+    create_external_creds(external_cred_configs=external_creds)
